124509d789895075b006c3e906e1fd0daf924a25
Brianna Woodruff <briannaewoodruff@gmail.com>
2022-05-30 18:11:48 -0500

    heroku server fix

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
 const serveStatic = require('serve-static')
 
-const PORT = process.env.PORT || 8080;
-const app = express();
+const path = require('path')
+const app = express()
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Serve up static assets
+//serve static /dist
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
+//to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+
+res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const PORT = process.env.PORT || 4000
+
 app.listen(PORT, () => {
-    console.log(`Listening at http://localhost:${PORT}`);
-});
+    console.log(`Example app listening at http://localhost:${PORT}`);
+})
