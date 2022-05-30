d391d459164829719a9a41d6ad0eacdd573586b1
Brianna Woodruff <briannaewoodruff@gmail.com>
2022-05-30 18:26:12 -0500

    json server

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const serveStatic = require('serve-static')
 const path = require('path')
 const app = express()
 
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+
+server.use(middlewares)
+server.use(router)
+server.listen(process.env.PORT || 5000, () => {
+    console.log('JSON Server is running')
+})
+
 //serve static /dist
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
@@ -13,7 +25,7 @@ app.get(/.*/, function (req, res) {
 res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
